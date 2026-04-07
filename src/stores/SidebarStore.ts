ad909453568209790bc6abd952351934d7f3a200
manilesrun <srunmanile@gmail.com>
2026-04-07 12:16:02 +1000

    fix: sidebar has so many states

--- src/stores/SidebarStore.ts
+++ src/stores/SidebarStore.ts
@@ -1,5 +1,4 @@
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
 
 type ActivePanel = "none" | "chats" | "admin";
 
@@ -15,22 +14,15 @@ type SidebarStore = {
   goNone: () => void;
 };
 
-export const useSidebarStore = create<SidebarStore>()(
-  persist(
-    (set) => ({
-      isOpen: false,
-      open: () => set({ isOpen: true }),
+export const useSidebarStore = create<SidebarStore>()((set) => ({
+  isOpen: false,
+  open: () => set({ isOpen: true }),
 
-      active: "none",
-      // openAdmin: () => set({ active: "admin" }),
-      openAdmin: () => set({ active: "admin", isOpen: true }),
-      openChatPanel: () => set({ active: "chats", isOpen: true }),
+  active: "none",
+  // openAdmin: () => set({ active: "admin" }),
+  openAdmin: () => set({ active: "admin", isOpen: true }),
+  openChatPanel: () => set({ active: "chats", isOpen: true }),
 
-      collapseChatPanel: () => set({ active: "none", isOpen: false }),
-      goNone: () => set({ active: "none", isOpen: false }),
-    }),
-    {
-      name: "sidebar-storage",
-    },
-  ),
-);
+  collapseChatPanel: () => set({ active: "none", isOpen: false }),
+  goNone: () => set({ active: "none", isOpen: false }),
+}));
