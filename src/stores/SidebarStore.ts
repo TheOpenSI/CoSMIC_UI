461e01cff5907ec03a6e0ffd7de3d59721796a06
manilesrun <srunmanile@gmail.com>
2026-04-26 23:46:45 +1000

    chore: make side bar state consistent

--- src/stores/SidebarStore.ts
+++ src/stores/SidebarStore.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
 type ActivePanel = "none" | "chats" | "admin";
 
@@ -14,15 +15,23 @@ type SidebarStore = {
   goNone: () => void;
 };
 
-export const useSidebarStore = create<SidebarStore>()((set) => ({
-  isOpen: false,
-  open: () => set({ isOpen: true }),
+export const useSidebarStore = create<SidebarStore>()(
+  persist(
+    (set) => ({
+      isOpen: false,
+      open: () => set({ isOpen: true }),
 
-  active: "none",
-  // openAdmin: () => set({ active: "admin" }),
-  openAdmin: () => set({ active: "admin", isOpen: true }),
-  openChatPanel: () => set({ active: "chats", isOpen: true }),
+      active: "none",
+      openAdmin: () => set({ active: "admin", isOpen: true }),
+      openChatPanel: () => set({ active: "chats", isOpen: true }),
 
-  collapseChatPanel: () => set({ active: "none", isOpen: false }),
-  goNone: () => set({ active: "none", isOpen: false }),
-}));
+      collapseChatPanel: () => set({ active: "none", isOpen: false }),
+      goNone: () => set({ active: "none", isOpen: false }),
+    }),
+    {
+      name: "sidebar-store",
+      // to make the sidebar working correctly, otherwise opens "chat panel" when users click admin page
+      storage: createJSONStorage(() => sessionStorage),
+    },
+  ),
+);
