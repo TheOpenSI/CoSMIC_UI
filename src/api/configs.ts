c730885741ef5da36a07a29e1497cc46e44bf2d9
Bing Tran <114546939+FaceWithDark@users.noreply.github.com>
2026-06-19 11:57:13 +1000

    feat: store modified configs data and rebuild CoSMIC at the same time
    There're linter changes in this commit:
    
    - Space to tab width (basically means 4 spaces)
    - Consistent code format for list of imports
    - Re-format value provided to some functions to make it a bit easier to
    read

--- src/api/configs.ts
+++ src/api/configs.ts
@@ -1,34 +1,54 @@
-import type { ConfigPayload, ConfigResponse } from "../types/configs";
+/// --- Core libraries --- ///
+
+
+/// --- Type hints --- ///
+import type {
+    ConfigPayload,
+    ConfigResponse,
+    ConfigUpdate
+} from "../types/configs";
+import type { CosmicUpdate } from "../types/cosmic";
+
+
+/// --- Internal libraries --- ///
 import { fetchWithAuth } from "./fetchWithAuth";
 
-// export async function updateConfig(payload: ConfigPayload) {
-//   const response = await fetchWithAuth(
-//     `${import.meta.env.VITE_API_BASE_URL}/config/update`,
-//     {
-//       method: "POST",
-//       body: JSON.stringify(payload),
-//     },
-//   );
 
-//   console.log(response);
-//   return response;
-// }
 
 export async function getConfigSettings(): Promise<ConfigResponse> {
-  return fetchWithAuth(
-    `${import.meta.env.VITE_API_DATABASE_URL}/api/v1/configs/`,
-  );
+    return fetchWithAuth(
+        `${import.meta.env.VITE_API_DATABASE_URL}/api/v1/configs/`,
+    );
 }
 
-export async function updateConfig(payload: ConfigPayload, id: string) {
-  const response = await fetchWithAuth(
-    `${import.meta.env.VITE_API_DATABASE_URL}/api/v1/configs/${id}`,
-    {
-      method: "PATCH",
-      body: JSON.stringify(payload),
-    },
-  );
 
-  console.log(response);
-  return response;
+export async function updateConfig(
+    payload: ConfigPayload,
+    id: string
+): Promise<ConfigUpdate> {
+    // Store modified configs via Configurations API first
+    const config_res: ConfigUpdate = await fetchWithAuth(
+        `${import.meta.env.VITE_API_DATABASE_URL}/api/v1/configs/${id}`,
+        {
+            method: "PATCH",
+            body: JSON.stringify(payload),
+        },
+    );
+
+
+    // Then rebuild CoSMIC stated configs only if above API calls succeed
+    if (config_res?.success) {
+        const cosmic_res: CosmicUpdate = await fetchWithAuth(
+            `${import.meta.env.VITE_API_BASE_URL}/api/v1/cosmic`,
+            {
+                method: "PATCH",
+            },
+        )
+
+        console.debug(cosmic_res);
+        return config_res;
+
+    } else {
+        return config_res;
+    }
 }
